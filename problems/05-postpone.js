0fa899b42cc374fa622a476c0c7c555238253166
JaceLehman228 <jacobthomaslehman@gmail.com>
2024-08-05 15:17:36 -0700

    All Problems Complete

--- problems/05-postpone.js
+++ problems/05-postpone.js
@@ -23,9 +23,14 @@ slowerGoodbye(); // prints 'bye' after 1750 ms
 ***********************************************************************/
 
 function postpone(cb, delay) {
-  // Your code here 
+  return () => {
+    setTimeout(cb, delay);
+  }
 }
-
+const sayGoodbye = () => console.log('bye');
+const slowerGoodbye = postpone(sayGoodbye, 1750);
+console.log(slowerGoodbye); // [Function]
+slowerGoodbye(); // prints 'bye' after 1750 ms
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
   module.exports = postpone;
